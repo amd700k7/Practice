ec7406fce9862d6b2fcfbc809ad538f099aa73a9
amd700k7 <amd700k7@gmail.com>
2014-01-17 23:49:26 -0500

    Initial commit
    tinkering with C#/Selenium

---
+++ Testing/Testing/Class1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace Testing
+{
+    public class Calculator
+    {
+        [TestCase(2, 2, 4)]
+        public void testaddition(int one, int two, int expected)
+        {
+            int testsum = addition(one, two);
+            Assert.AreEqual(expected, testsum, "failed");
+        }
+        public int addition(int a, int b)
+        {
+            int sum = 0;
+            sum = a + b;
+
+            return sum;
+
+        }
+
+    }
+    public class Browsertest
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void init()
+        {
+            driver = new FirefoxDriver();
+        }
+        [TearDown]
+        public void destroy()
+        {
+            driver.Dispose();
+        }
+
+        [Test]
+        public void simplesearch()
+        {
+            //Given
+            driver.Navigate().GoToUrl("https://www.google.com");
+            Assert.IsTrue(driver.FindElement(By.Id("hplogo")).Displayed, "logo not found");
+
+            //When
+            driver.FindElement(By.Name("q")).Clear();
+            driver.FindElement(By.Name("q")).SendKeys("oiuyyg976f867f865fdy6t7fd65d5674sd56rdytuf");
+            driver.FindElement(By.Name("btnG")).Click();
+            Console.WriteLine("Hello World!");
+            //Then
+            //System.Threading.Thread.Sleep(10000);
+            Assert.IsTrue(ClassElementDisplayed("g", driver), "results not found");
+        }
+
+        public bool ClassElementDisplayed(string classname, IWebDriver driver)
+        {
+            bool foundelement = false;
+            try
+            {
+                foundelement = driver.FindElement(By.ClassName(classname)).Displayed;
+
+            }
+            catch (NoSuchElementException)
+            {
+                foundelement = false;
+            }
+
+            return foundelement;
+
+        }
+
+
+    }
+}
